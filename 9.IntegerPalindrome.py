4c3b2042f29bf464bec3ca1ca9a8c2c2b82791cd
MyMishi <34791009+MyMishi@users.noreply.github.com>
2018-07-10 01:49:27 -0700

    Update 9.IntegerPalindrome.py

--- 9.IntegerPalindrome.py
+++ 9.IntegerPalindrome.py
@@ -1,11 +1,8 @@
-##############
+"""
 
-#Problem -
-#Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
 
-#################
-
-#Solution -
+"""
 
 def isPalindrome(self, x):
         copyX = x
